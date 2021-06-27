cb6564110c8ff1b96875bc2368070a2a8d53442f
--replace-all <jgaito@ucsc.edu>
2021-06-27 01:36:23 -0500

    hopefully fixing things to work

--- src/prefabs/Sock.js
+++ src/prefabs/Sock.js
@@ -8,7 +8,7 @@ class Sock extends Phaser.GameObjects.Sprite {
     }
     
     update() {
-        //move spaceship left
+        //move sock left
         this.x -= this.moveSpeed;
         // wrap around from left edge to right edge
         if(this.x <= 0 - this.width) {
