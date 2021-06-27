8174926dddace7bf0d64007eec8630f5f879ef5c
--replace-all <jgaito@ucsc.edu>
2021-06-27 09:59:42 -0500

    hopefully makes things work

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,9 +4,9 @@ class Menu extends Phaser.Scene {
     }
     preload() {
         // load audio
-        this.load.audio('sfx_select', './assets/blip_select122.wav');
-        this.load.audio('sfx_explosion', './assets/explosion388.wav');
-        this.load.audio('sfx_rocket', './assets/rocket_shot1.wav');
+        this.load.audio('sfx_select', './assets/blip_select12.wav');
+        this.load.audio('sfx_explosion', './assets/explosion38.wav');
+        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
       }
     create() {
       // menu text configuration
